4f60c378a8db17832251d2eef931d12904767784
Weiwei Fu <weiweif@uci.edu>
2020-08-11 20:22:48 -0700

    Update ConfdTdZ.py

--- ConfdTdZ.py
+++ ConfdTdZ.py
@@ -17,6 +17,84 @@ the entire time period of the GLODAP data.
 need to be temporal with a single time slice.
 
 """
+def sc_fit(temp=None,modn='CESM2',X=None,Y=None,shift_xy=None,latbnd=slice(0,50),tsp='default',coef2d=None,y1=1.8,y2=4.5):
+  from sklearn.linear_model import  LinearRegression
+
+  tL={'default':slice(0,None),'1994s':slice(14,27),'1980s':slice(0,10),'1990s':slice(10,20),'2000s':slice(20,30)}
+  def _dindx(v,S0=200,Sc=1000):
+    # adep = v['lev_bnds'][:,1] - v['lev_bnds'][:,0]
+    # find the index for 0 and 3000 m
+    i0 = np.argmin(abs(v['lev']- S0))
+    ic = np.argmin(abs(v['lev']- Sc))
+    print('level ',i0,'-',ic,' in ',v['lev'][i0],'-',v['lev'][ic],'m')
+    d1 = slice(i0,ic) # CMIP 0-3000m
+    # return adep, d1
+    return i0,ic
+  def _getslope(X,Y3D):
+    sh = Y3D.shape
+    coef = copy.deepcopy(Y3D[0,:,:])
+    print('the shape of Y3D is',sh)
+    for j in range(0,sh[-2]):
+      for i in range(0,sh[-1]):
+        model = LinearRegression()
+        Y = Y3D[:,j,i]
+        output = model.fit(X[:,None],Y)
+        coef[j,i] = output.coef_
+    return coef
+
+  if temp is not None:
+    i0,ic = _dindx(temp)
+    lev = temp['lev']
+    X = lev[i0:ic+1]
+    if temp['thetao'].ndim == 4:
+      Y = temp['thetao'][tL[tsp],:,latbnd,:].mean(axis=(0,2,3))[i0:ic+1]
+      Y3D = temp['thetao'][tL[tsp],i0:ic+1,:,:].mean(axis=(0))
+    else:
+      Y = temp['thetao'][:,latbnd,:].mean(axis=(1,2))[i0:ic+1]
+  else:
+    print('X and Y must be specified')
+  print(X.shape,Y.shape)
+
+  print('try to get coefs')
+  if coef2d is not None:
+    ccc = _getslope(X,Y3D)
+  else:
+    ccc = None
+
+  model = LinearRegression()
+
+  plt.close('all')
+  fig, ax = plt.subplots(nrows=1,ncols=1,figsize=(5,3))
+  fig.tight_layout()
+  if shift_xy is None:
+    ax.plot(X,Y,'.')
+    output = model.fit(X[:,None],Y)
+  else:
+    ax.plot(Y,X,'.')
+    output = model.fit(Y[:,None],X)
+  # print model
+  print('Model intercept:',output.intercept_)
+  print('Model slope:',output.coef_)
+  if shift_xy is None:
+    R2 = output.score(X[:,None],Y)
+    print('R2:',R2)
+    xfit = np.linspace(X.min(),X.max(),1000)
+    yfit = output.predict(xfit[:,None])
+    ax.plot(xfit,yfit,'r')
+  else:
+    R2 = output.score(X[:,None],Y)
+    print('R2:',R2)
+    xfit = np.linspace(Y.min(),Y.max(),1000)
+    yfit = output.predict(xfit[:,None])
+    ax.plot(xfit,yfit,'r')
+  ax.xaxis.set_tick_params(labelsize=14)
+  ax.yaxis.set_tick_params(labelsize=14)
+  # ax.set_title(modn,fontsize=14)
+  ax.text(np.median(xfit),np.median(yfit),'dT/dz='+'{:.4f}'.format(model.coef_[0])+'\nR2='+'{:.2f}'.format(R2),fontsize=14)
+  ax.set_ylim([y1, y2])
+  fig.suptitle(modn,fontsize=14)
+  return fig,ccc
+
 def GetSlope(v):
     # loop over unmasked cells and compute slope wrt depth
     def _dindx(v,S0=200,Sc=1000):
